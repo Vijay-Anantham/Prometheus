ab0639d44440b416022db39fca42ee32623c9bfb
vijaysek <vijaysek@cisco.com>
2023-11-13 12:32:47 +0530

    remaining files for intraday stock fetch

--- services/stockApi.go
+++ services/stockApi.go
@@ -4,55 +4,72 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 )
 
-type StockPrice struct {
-	Symbol string  `json:"symbol"`
-	Price  float64 `json:"price"`
+const (
+	apiKey      = "TOB0OSLJF393DSV0"
+	stockSymbol = "CSCO" // Replace with the stock symbol you want to monitor
+	interval    = 1      // Interval in minutes
+)
+
+type IntradayResponse struct {
+	MetaData   map[string]string `json:"Meta Data"`
+	TimeSeries map[string]Price  `json:"Time Series (1min)"`
 }
 
-func ApiHandler(w http.ResponseWriter, r *http.Request) {
-	// Make an API call to Alpha Vantage
+type Price struct {
+	Open   string `json:"1. open"`
+	High   string `json:"2. high"`
+	Low    string `json:"3. low"`
+	Close  string `json:"4. close"`
+	Volume string `json:"5. volume"`
+}
 
-	resp, err := http.Get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=TOB0OSLJF393DSV0")
+func GetStockPrice(w http.ResponseWriter, r *http.Request) {
+	err := fetchAndPrintStockPrices(w)
 	if err != nil {
-		log.Fatal(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func fetchAndPrintStockPrices(w http.ResponseWriter) error {
+	url := fmt.Sprintf("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=%s&interval=1min&apikey=%s", stockSymbol, apiKey)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
 	}
 	defer resp.Body.Close()
 
-	// Read the API response body
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	// Parse the JSON response
-	var data struct {
-		GlobalQuote struct {
-			Symbol string `json:"01. symbol"`
-			Price  string `json:"05. price"`
-		} `json:"Global Quote"`
-	}
+	var data IntradayResponse
 	err = json.Unmarshal(body, &data)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	// Create a StockPrice struct with the parsed data
-	stockPrice := StockPrice{
-		Symbol: data.GlobalQuote.Symbol,
-	}
-	fmt.Sscanf(data.GlobalQuote.Price, "%f", &stockPrice.Price)
-
-	// Convert the StockPrice struct to JSON
-	jsonData, err := json.Marshal(stockPrice)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Set the Content-Type header and send the JSON response back to the client
+	latestData := getLatestPrice(data.TimeSeries)
+	fmt.Sprint("Fetching stock prices: \n")
+	response := fmt.Sprintf(`{"Stock name": "%s", "latest_price": "%s"}`, stockSymbol, latestData.Close)
+	fmt.Println("Response:", string(response))
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsonData)
+	w.Write([]byte(response))
+
+	return nil
+}
+
+func getLatestPrice(timeSeries map[string]Price) Price {
+	// Extract the latest stock price from the response
+	var latestData Price
+	for _, v := range timeSeries {
+		latestData = v
+		break
+	}
+	return latestData
 }
